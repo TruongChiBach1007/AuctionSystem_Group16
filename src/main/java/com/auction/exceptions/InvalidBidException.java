5ccce5dfdd3dfaa30c3fb2010308d8875898ded9
TruongChiBach1007 <25023442@vnu.edu.vn>
2026-04-01 13:31:36 +0700

    Setup model

--- src/main/java/com/auction/exceptions/InvalidBidException.java
+++ src/main/java/com/auction/exceptions/InvalidBidException.java
@@ -1,6 +1,6 @@
 package com.auction.exceptions;
 public class InvalidBidException extends RuntimeException {
     public InvalidBidException(String message){
-
+        super(message);
     }
 }
--- src/main/java/com/auction/model/Auction.java
+++ src/main/java/com/auction/model/Auction.java
@@ -1,16 +1,28 @@
 package com.auction.model;
 import com.auction.autobid.AutoBid;
+import com.auction.exceptions.InvalidBidException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Auction {
     private double currentPrice;
     private Bidder highestBidder;
-    private boolean closed = false;
+    private boolean closed = false; // trạng thái phiên đang mở
 
-    private final Object lock = new Object();
+    private final Object lock = new Object(); //synchronized
+
+    private List<AutoBid> autoBids = new ArrayList<>();
 
     public Auction(double starPrice) {
         this.currentPrice = starPrice;
     }
+    public void registerAutoBid(AutoBid autoBid) {
+        synchronized (lock) {
+            autoBids.add(autoBid);
+        }
+    }
+    //
     public boolean placeBid(Bid bid) {
         synchronized (lock) { // chỉ có 1 thread đợc xảy ra tại thời điểm đó
             if (closed) {
@@ -28,6 +40,31 @@ public class Auction {
 
             return true;
         }
+    }
+    private void processAutoBids() {
+        boolean updated;
+
+        do {
+            updated = false;
+
+            for (AutoBid auto : autoBids) {
+
+                if (auto.getBidder() == highestBidder) continue;
+
+                double nextBid = currentPrice + auto.getIncrement();
+
+                if (nextBid <= auto.getMaxBid()) {
+
+                    currentPrice = nextBid;
+                    highestBidder = auto.getBidder();
+
+                    System.out.println("AutoBid: " + currentPrice + " by " + highestBidder.getName());
+
+                    updated = true;
+                }
+            }
+
+        } while (updated);
     }
 
     public void closeAuction() {
