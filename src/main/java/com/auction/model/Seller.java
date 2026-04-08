fb7fbd10f44112096036e255f8b032fde4326948
vietdung <nguyenvietdungk7@gmail.com>
2026-04-08 14:00:31 +0700

    add user

---
+++ src/main/java/com/auction/model/Seller.java
@@ -0,0 +1,29 @@
+package com.auction.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Seller extends User {
+    // Thuộc tính riêng: Danh sách các tên sản phẩm mà người bán này đang đăng [cite: 39]
+    private List<String> listedItems;
+
+    public Seller(int id, String username, String password, String fullName, String email) {
+        // Gọi constructor của lớp cha User
+        super(id, username, password, fullName, email);
+        this.listedItems = new ArrayList<>();
+    }
+
+    // Chức năng: Thêm sản phẩm mới vào danh sách quản lý của người bán [cite: 40]
+    public void addListedItem(String itemName) {
+        listedItems.add(itemName);
+    }
+
+    public List<String> getListedItems() {
+        return listedItems;
+    }
+    @Override
+    public void displayRoleInfo() {
+        System.out.println("Vai trò: Người bán (Seller)");
+        System.out.println("Số lượng sản phẩm đã đăng: " + listedItems.size());
+    }
+}
