ead0d0d3b31a5be1a82adeeceecd2815a930b170
vietdung <nguyenvietdungk7@gmail.com>
2026-05-06 15:41:41 +0700

    ui

--- src/main/java/com/auction/Launcher.java
+++ src/main/java/com/auction/Launcher.java
@@ -1,8 +1,10 @@
 package com.auction;
 
+import javafx.application.Application;
+
 public class Launcher {
     public static void main(String[] args) {
-        // Gọi hàm main của AuctionApp từ đây để lách luật kiểm tra của JavaFX
-        AuctionApp.main(args);
+        // Sử dụng Application.launch thay vì gọi main trực tiếp
+        Application.launch(AuctionApp.class, args);
     }
 }
